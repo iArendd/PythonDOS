286022fd61d661a809aafd93089df45280d2e993
Arend <bi_arend@hotmail.com>
2022-08-17 20:24:17 -0300

    Step 1 - Test

---
+++ wfind.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set fileName = %1
+
+python "C:\xampp\htdocs\faculdade\sistemasOperacionais\trabalho01\wfinder.py" %CD% %fileName%
+
+pause
+
+cls
---
+++ wfinder.py
@@ -0,0 +1,8 @@
+import sys
+import os
+from pathlib import Path
+
+searched_file = sys.argv[2]
+root_dir = sys.argv[1]
+
+print("\n ----- searching for files containg string: " + searched_file)
