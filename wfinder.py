c46277d3cbad4083ad7f062d1c48e5e233f3200d
Arend <bi_arend@hotmail.com>
2022-08-17 21:37:38 -0300

    Step 2 - Test

--- wfinder.py
+++ wfinder.py
@@ -2,7 +2,25 @@ import sys
 import os
 from pathlib import Path
 
-searched_file = sys.argv[2]
 root_dir = sys.argv[1]
+searched_file = sys.argv[2]
 
-print("\n ----- searching for files containg string: " + searched_file)
+print("\n ----- searching for files containg string: " + searched_file + "-----\n")
+
+def loopdirs(folder):
+
+    try:
+
+        for root, subFolder, files in os.walk(folder):
+
+            for file in files:
+
+                if searched_file in file:
+
+                    print(root + "--" + file)
+
+    except:
+
+        print("\nExecution stopped!\n")
+
+loopdirs(root_dir)
