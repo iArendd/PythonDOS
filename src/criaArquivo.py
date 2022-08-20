32873b4726373ade0feeb587f67b6aa5de3f6ca6
Arend <bi_arend@hotmail.com>
2022-08-20 12:55:37 -0300

    createFile - Step1

--- src/criaArquivo.py
+++ src/criaArquivo.py
@@ -2,7 +2,19 @@ import os
 
 def gerarArquivo():
 
-    arquivo = open("contatos.txt", "a")
-    #arquivo.write("Ola, mundo!")
+    gerarArquivoEscolha = input(str("\nO arquivo que você deseja criar será inserido na pasta raiz, você deseja inserir em uma pasta específica? Digite SI para sim ou NO para não.\n"))
 
-gerarArquivo()
+    if gerarArquivoEscolha == "NO":
+        
+        nomeArquivo = input(str("\nDigite o nome do arquivo a ser criado:\n"))
+        formatoArquivo = input(str("\nDigite a extenção do tipo do arquivo a ser criado:\n"))
+
+        arquivo = open(nomeArquivo+"."+formatoArquivo, "a")
+
+        print("O arquivo {}.{} foi criado com sucesso.".format(nomeArquivo, formatoArquivo))
+
+    else:
+        print("Nada")
+
+    #arquivo = open("pastasCriadas/contatos.txt", "a")
+    #arquivo.write("Ola, mundo!")
