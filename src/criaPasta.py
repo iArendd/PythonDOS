7346f6dbf1dd0ae19e2bbaab4bb45ac593d2e78d
Arend <bi_arend@hotmail.com>
2022-08-20 12:10:29 -0300

    Criando arquivo help

--- src/criaPasta.py
+++ src/criaPasta.py
@@ -4,7 +4,7 @@ def gerarPasta():
     nomePasta = str(input("Digite o nome da pasta a ser criada\n"))
     os.makedirs('pastasCriadas/' + nomePasta)
 
-    print("A pasta "+nomePasta+" foi criada com sucesso dentro do diretório específico chamado 'pastasCriadas'")
+    print("\nA pasta "+nomePasta+" foi criada com sucesso dentro do diretório específico chamado 'pastasCriadas'\n")
 
 
 
