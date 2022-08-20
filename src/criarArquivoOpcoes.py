a1fd32b1faf37d86a3b363fdc70e21de3a31090f
Arend <bi_arend@hotmail.com>
2022-08-20 13:51:28 -0300

    createFile - Step4
    opção NO: Escrever no arquivo

--- src/criarArquivoOpcoes.py
+++ src/criarArquivoOpcoes.py
@@ -10,10 +10,26 @@ def opcaoNAO():
 
     print("O arquivo {}.{} foi criado com sucesso.".format(nomeArquivo, formatoArquivo))
 
+    print("\n===============================================================\n")
+
+    escreverArquivo = input(str("Você deseja escrever alguma informação dentro do arquivo? Digite S para sim ou N para não:\n"))
+
+    if escreverArquivo == "S":
+
+        descricaoArquivo = input(str("Escreva o que deve ser inserido no arquivo:\n"))
+        arquivo.write(descricaoArquivo+"\n")
+
+        print("A descrição '{}' foi inserida com sucesso.\n".format(descricaoArquivo))
+
+    elif escreverArquivo == "N":
+        print("Programa encerrado.")
+        print("===============================================================\n")
+
+
 #---------------------------------------------------------------------------------
 
 def opcaoSIM():
-    
+
     nomeDiretorio = input(str("\nDigite o diretório da pasta que você deseja criar o arquivo. Exemplo: 'primeiraPasta/segundaPasta/terceiraPasta/' \n"))
 
     nomeArquivo = input(str("\nDigite o nome do arquivo a ser criado:\n"))
